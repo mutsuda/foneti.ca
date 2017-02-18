534aa115ca20a124c824724f818df54e0565e57a
Masumi Mutsuda Zapater <mutsuda@gmail.com>
2017-02-18 18:56:53 +0100

    separated functions

---
+++ functions.js
@@ -0,0 +1,16 @@
+$('#search').keydown(function(){
+  $.getJSON("data.json",function(data){
+  var search = $('#search').val();
+  var regex = new RegExp(search, 'i');
+  var output;
+    $.each(data, function(key, val){
+      if(val.word.search(regex) != -1) {
+        output += "<tr>";
+        output += "<td id='"+key+"'>"+val.word+"</td>";
+        output += "</tr>";
+      }
+    });
+    $('tbody').html(output);
+  });
+})
+
